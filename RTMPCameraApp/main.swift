44d78ff5155858404e5aa8252b68d207336e4e8c
ourongyime <9450915@qq.com>
2026-07-20 13:03:29 +0800

    v1.0.11: 修复闪退 - UIApplicationMain第三个参数应为nil而非delegate

--- RTMPCameraApp/main.swift
+++ RTMPCameraApp/main.swift
@@ -1,10 +1,10 @@
-import UIKit
+﻿import UIKit
 
 // main.swift - RTMPCameraApp 程序入口
 
 UIApplicationMain(
     CommandLine.argc,
     CommandLine.unsafeArgv,
-    NSStringFromClass(RTMPAppDelegate.self),
+    nil,
     NSStringFromClass(RTMPAppDelegate.self)
 )
